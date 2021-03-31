2affbe2557d589c54666ea95624d7039e6d8d746
arthurs14 <arthurcs426@gmail.com>
2021-03-31 16:57:12 -0700

    made a new card that displays the number the user typed in

--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -11,6 +11,7 @@ import {
 
 import Card from '../components/Card';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 import Colors from '../../constants/colors';
 
 const StartGameScreen = () => {
@@ -40,12 +41,19 @@ const StartGameScreen = () => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue('');
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>{`Chosen Number: ${selectedNumber}`}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You Selected</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button title="START GAME" />
+      </Card>
+    );
   }
 
   return (
@@ -118,6 +126,9 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: 'center',
+  },
+  summaryContainer: {
+    marginTop: 20,
   },
 });
 
