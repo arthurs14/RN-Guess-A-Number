d9c72573859fb233d25e813e6f727b8401f8bed8
arthurs14 <arthurcs426@gmail.com>
2021-04-07 20:04:17 -0700

    created a new file to hold text

---
+++ src/components/BodyText.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const BodyText = props => <Text>{props.children}</Text>;
+
+const styles = StyleSheet.create({});
+
+export default BodyText;
