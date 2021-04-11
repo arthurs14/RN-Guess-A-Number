7534fa9be00026ee7bb41e36d9da77ac6a6c0c5e
arthurs14 <arthurcs426@gmail.com>
2021-04-11 15:47:09 -0700

    got rid of unused imports and added a png for the game over screen

--- src/screens/GameOverScreen.js
+++ src/screens/GameOverScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Image } from 'react-native';
 
+import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
 
 const GameOverScreen = ({ rounds, userNumber, restart }) => {
   return (
     <View style={styles.screen}>
-      <BodyText>Game Over!</BodyText>
+      <TitleText>Game Over!</TitleText>
+      <Image source={require('../assets/success.png')} />
       <BodyText>{`Number of Rounds: ${rounds}`}</BodyText>
       <BodyText>{`Number was: ${userNumber}`}</BodyText>
       <View style={styles.button}>
