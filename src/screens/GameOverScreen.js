a13db2815c76c8bad30d1a95615a5dd22300a6dd
arthurs14 <arthurcs426@gmail.com>
2021-04-19 15:46:17 -0700

    changes to the image container when in landscape mode

--- src/screens/GameOverScreen.js
+++ src/screens/GameOverScreen.js
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
-    paddingVertical: 10,
+    paddingVertical: 20,
   },
   button: {
     marginTop: 10,
