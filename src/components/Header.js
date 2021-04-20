5bdc39260e5d7bda0405acfbb600ac209bb61d70
arthurs14 <arthurcs426@gmail.com>
2021-04-20 16:20:35 -0700

    made it more leaner with how we use platform

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,15 @@ import TitleText from '../components/TitleText';
 
 const Header = ({ title }) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.header,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <TitleText style={styles.title}>{title}</TitleText>
     </View>
   );
@@ -17,11 +25,16 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+  },
+  headerIOS: {
+    backgroundColor: 'white',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: Colors.primary,
   },
   title: {
     color: Platform.OS === 'ios' ? Colors.primary : 'white',
