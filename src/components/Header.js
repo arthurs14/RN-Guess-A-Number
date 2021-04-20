d2b02feb16b85109b5e23eb0f5e39ce31e58d978
arthurs14 <arthurcs426@gmail.com>
2021-04-20 16:08:57 -0700

    added Platform to help determine which styles to configure depending on device

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 
 import Colors from '../../constants/colors';
 import TitleText from '../components/TitleText';
@@ -7,7 +7,7 @@ import TitleText from '../components/TitleText';
 const Header = ({ title }) => {
   return (
     <View style={styles.header}>
-      <TitleText>{title}</TitleText>
+      <TitleText style={styles.title}>{title}</TitleText>
     </View>
   );
 };
@@ -17,9 +17,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+  },
+  title: {
+    color: Platform.OS === 'ios' ? Colors.primary : 'white',
   },
 });
 
